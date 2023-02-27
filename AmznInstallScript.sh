f8b12101044190db723abed4f1c545d3b30b26d2
Alexander Bedrossian <43423796+aab641@users.noreply.github.com>
2023-02-27 17:50:11 -0500

    Add files via upload

--- AmznInstallScript.sh
+++ AmznInstallScript.sh
@@ -26,8 +26,8 @@ else
 	echo Dependency-check already downloaded!
 fi
 
-ping git.amazon.com -c 1 2> /dev/null | grep '1 received' &> /dev/null
-if [ $? != 0 ]; then
+
+if [ ping git.amazon.com -c 1 2> /dev/null | grep '1 received' ]; then
 	git clone ssh://git.amazon.com/pkg/VAPTPublic &> /dev/null
 else
 	echo Error: VAPT Public not downloaded! You are likely not on an Amazon Cloud Desktop!!
