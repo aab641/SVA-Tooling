7e197f84b06614090a80fee21ec2aeb5e29db059
Alexander Bedrossian <43423796+aab641@users.noreply.github.com>
2023-02-27 22:57:41 -0500

    Add files via upload

--- AmznScanScript.sh
+++ AmznScanScript.sh
@@ -7,22 +7,51 @@ fi
 if [ $# -eq 4 ];
 then
     echo "ProjectName: $1"
+    echo
     echo "Hosts: $2"
+    for word in $(cat $2); do echo $word; done
+    echo
     echo "Service Roles: $3"
+    echo
     echo "Code Package URLs: $4"
+    echo
 else
     echo "Syntax is as follows: AmznScanScript.sh <projectName> <hosts.txt> <serviceroles.txt> <codepackages.txt>"
     exit 1
 fi
 
 echo "Making project directory."
-if [ ! -d $1 ]; then
-	mkdir $1
+if [ ! -d "$1" ]; then
+	mkdir "$1"
 else
-	echo "Directory already exists."
+	echo "Directory ~/$1 already exists."
 fi
+echo "Making nmap directory."
+if [ ! -d "$1/nmap" ]; then
+	mkdir "$1/nmap"
+else
+	echo "Directory ~/$1/nmap/ already exists."
+fi
+echo
+echo "Making testssl directory."
+if [ ! -d "$1/testssl" ]; then
+	mkdir "$1/testssl"
+else
+	echo "Directory ~/$1/testssl/ already exists."
+fi
+echo
 
 echo "Performing TCP nmap scan."
-nmap -p1-65535 -sS -iL $2 -oN "$1/nmap/$1_TCP_nmap.txt" -oX "$1/nmap/$1_TCP_nmap.xml"
+for word in $(cat $2); do mkdir $1/nmap/$word; sudo nmap -p0- -A -T4 -sS --max-retries 0 $word -oN "$1/nmap/$word/$1-$word-TCP_nmap.txt" -oX "$1/nmap/$word/$1-$word-TCP_nmap.xml"; done
+echo 
+
 echo "Performing UDP nmap scan."
-nmap -p1-65535 -sU -iL $2 -oN "$1/nmap/$1_UDP_nmap.txt" -oX "$1/nmap/$1_UDP_nmap.xml" 
+for word in $(cat $2); do mkdir $1/nmap/$word; sudo nmap -p0- -A -T4 -sU --max-retries 0 $word -oN "$1/nmap/$word/$1-$word-UDP_nmap.txt" -oX "$1/nmap/$word/$1-$word-UDP_nmap.xml"; done
+echo
+
+echo "Performing ssltest."
+for word in $(cat $2); do bash testssl.sh/testssl.sh --ip one --parallel --outprefix "$1/testssl/" --html $word; done
+echo
+
+echo "Done."
+
