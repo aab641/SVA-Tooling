0e65abedc497340059ed013cecc95e44d00178a6
Alexander Bedrossian <43423796+aab641@users.noreply.github.com>
2023-03-02 00:14:48 -0500

    Add files via upload

--- AmznScanScript.sh
+++ AmznScanScript.sh
@@ -4,17 +4,35 @@ then
     echo "No arguments supplied."
 fi
 
-if [ $# -eq 4 ];
+if [ $# -eq 3 ];
 then
     echo "ProjectName: $1"
     echo
     echo "Hosts: $2"
-    for word in $(cat $2); do echo $word; done
+    for word in $(cat $2);
+    do 
+    	echo $word;
+    done
     echo
-    echo "Service Roles:"
-    echo
+        echo "Code Package URLs: $3"
+    for word in $(cat $3); do
+    	echo "$word";
+    done
+    echo 
+    echo "Fixing code package URLs"
+    # Assign the filename
+    
+    search="https:\/\/code.amazon.com\/packages\/"
+    replace="ssh:\/\/git.amazon.com\/pkg\/"
+    sed -i "s/$search/$replace/g" $3
+    search="\/trees\/mainline/"
+    replace=" "
+    sed -i "s/$search/$replace/g" $3
+    
     echo "Code Package URLs: $3"
-    for word in $(cat $3); do echo $word; done
+    for word in $(cat $3); do
+    	echo "$word";
+    done
     echo
 else
     echo "Syntax is as follows: AmznScanScript.sh <projectName> <hosts.txt> <codepackages.txt>"
@@ -49,7 +67,25 @@ else
 fi
 echo
 
+echo "Downloading code packages."
+echo "Making folder '/code-packages/' in directory '/$1'."
+if [ ! -d "$1/code-packages" ]; then
+	mkdir "$1/code-packages"
+else
+	echo "Directory ~/$1/code-packages/ already exists."
+fi
+echo
+for word in $(cat $3); do
+	cd "$1/code-packages";
+	git clone "$word";
+	cd "../..";
+done
+
+exit 1;
+
+echo "Performing ScoutSuite scan."
 if [ $AWS_ACCESS_KEY_ID ]; then
+	echo "AWS ACCESS KEYS DETECTED!";
 	echo "Making scoutsuite directory."
 	if [ ! -d "$1/scoutsuite" ]; then
 		mkdir "$1/scoutsuite"
@@ -57,8 +93,13 @@ if [ $AWS_ACCESS_KEY_ID ]; then
 		echo "Directory ~/$1/scoutsuite/ already exists."
 	fi
 	echo
-	python3 ScoutSuite/scout.py aws --report-dir "$1/scoutsuite" --report-name "$1-scoutsuite-report" --access-keys --access-key-id $AWS_ACCESS_KEY_ID --secret-access-key $AWS_SECRET_ACCESS_KEY --session-token $AWS_SESSION_TOKEN
+	python3 ScoutSuite/scout.py aws --report-dir "$1/scoutsuite" --report-name "$1-scoutsuite_report" --access-keys --access-key-id $AWS_ACCESS_KEY_ID --secret-access-key $AWS_SECRET_ACCESS_KEY --session-token $AWS_SESSION_TOKEN
+else
+	echo "AWS ACCESS KEYS NOT DETECTED! SKIPPING SCOUTSUITE AUDIT!"
+	echo 
 fi
+
+
 
 echo "Performing TCP nmap scan."
 for word in $(cat $2); do mkdir $1/nmap/$word; sudo nmap -p0- -A -T4 -sS --max-retries 0 $word -oN "$1/nmap/$word/$1-$word-TCP_nmap.txt" -oX "$1/nmap/$word/$1-$word-TCP_nmap.xml"; done
@@ -75,10 +116,10 @@ echo
 echo "Performing slowhttptests."
 for word in $(cat "$2"); do 
 mkdir $word
-./slowhttptest/bin/slowhttptest -c 40000 -X -g -o $1/slowhttptest/$word/$word-slowread -r 200 -w 512 -y 1024 -n 5 -z 32 -k 3 -u https://$word -p 3
-./slowhttptest/bin/slowhttptest -c 500 -H -g -o $1/slowhttptest/$word/$word-slowhttp -i 10 -r 200 -t GET -u https://$word -x 24 -p 3
-./slowhttptest/bin/slowhttptest -c 2000 -B -g -o $1/slowhttptest/$word/$word-slowbody -i 100 -s 7000 -r 200 -t GET -u https://$word -x 24 -p 3
-./slowhttptest/bin/slowhttptest -R -u https://$word -t HEAD -c 1000 -a 10 -b 3000 -r 500 -i 10  -g -o $1/slowhttptest/$word/$word-rangeheader
+./slowhttptest/bin/slowhttptest -c 40000 -X -g -o $1/slowhttptest/$word/$1-$word-slowread -r 200 -w 512 -y 1024 -n 5 -z 32 -k 3 -u https://$word -p 3
+./slowhttptest/bin/slowhttptest -c 500 -H -g -o $1/slowhttptest/$word/$1-$word-slowhttp -i 10 -r 200 -t GET -u https://$word -x 24 -p 3
+./slowhttptest/bin/slowhttptest -c 2000 -B -g -o $1/slowhttptest/$word/$1-$word-slowbody -i 100 -s 7000 -r 200 -t GET -u https://$word -x 24 -p 3
+./slowhttptest/bin/slowhttptest -R -u https://$word -t HEAD -c 1000 -a 10 -b 3000 -r 500 -i 10  -g -o $1/slowhttptest/$word/$1-$word-rangeheader
 done
 echo
 
